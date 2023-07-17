f6ba8f1ff36f2c8d6fb8c6d98b2cbd5a77506b47
dci-student <fredy.montalvo@dci.student>
2023-07-17 12:04:11 +0200

    Updated

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {// cuando se cargue el documento ejecuta el codigo
-  const buttonNumm = document.querySelectorAll(".number");//  seleccionamos todos los botones que tengan la clase number     
-  const buttonOper = document.querySelectorAll(".operator");//  seleccionamos todos los botones que tengan la clase operator
+document.addEventListener("DOMContentLoaded", function () {
+  // cuando se cargue el documento ejecuta el codigo
+  const buttonNumm = document.querySelectorAll(".number"); //  seleccionamos todos los botones que tengan la clase number
+  const buttonOper = document.querySelectorAll(".operator"); //  seleccionamos todos los botones que tengan la clase operator
   const buttonEqual = document.querySelector(".equal");
   const buttonErase = document.querySelector(".erase");
   const buttonEraseAll = document.querySelector(".erase-all");
   const textSuperiorValue = document.querySelector(".superior_value");
   const textInferiorValue = document.querySelector(".inferior_value");
-
-
 
   class Calculator {
     constructor(textInferiorValue, textSuperiorValue) {
@@ -15,33 +14,33 @@ document.addEventListener("DOMContentLoaded", function () {// cuando se cargue e
       this.textSuperiorValue = textSuperiorValue;
       this.inferiorValue = "";
       this.superiorValue = "";
-      this.operator = undefined;//para guardar la operacion que se va a realizar
+      this.operator = undefined; //para guardar la operacion que se va a realizar
     }
     addNumber(number) {
-      if (number === "." && this.inferiorValue.includes(".")) return;//si el numero es un punto y el valor inferior incluye un punto no hace nada.
-      this.inferiorValue += number;//concatena el numero que se presiona
+      if (number === "." && this.inferiorValue.includes(".")) return; //si el numero es un punto y el valor inferior incluye un punto no hace nada.
+      this.inferiorValue += number; //concatena el numero que se presiona
     }
     updateDisplay() {
       this.textInferiorValue.textContent = this.inferiorValue;
       this.textSuperiorValue.textContent = this.superiorValue;
     }
     erase() {
-      this.inferiorValue = this.inferiorValue.slice(0, -1);//elimina el ultimo elemento de la cadena
+      this.inferiorValue = this.inferiorValue.slice(0, -1); //elimina el ultimo elemento de la cadena
     }
     chooseOperation(operator) {
-      if (this.inferiorValue === "") return;//    
+      if (this.inferiorValue === "") return; //
       if (this.superiorValue !== "") {
-        this.compute();//si el valor superior no esta vacio realiza la operacion
+        this.compute(); //si el valor superior no esta vacio realiza la operacion
       }
-      this.operator = operator;//guarda el operador
-      this.superiorValue = this.inferiorValue;//guarda el valor inferior en el superior
-      this.inferiorValue = "";//limpia el valor inferior
+      this.operator = operator; //guarda el operador
+      this.superiorValue = this.inferiorValue; //guarda el valor inferior en el superior
+      this.inferiorValue = ""; //limpia el valor inferior
     }
     compute() {
       let result;
       let convertSuperiorValue = parseFloat(this.superiorValue);
       let convertInferiorValue = parseFloat(this.inferiorValue);
-      if (isNaN(convertSuperiorValue) || isNaN(convertInferiorValue)) return;//si no es un numero no hace nada
+      if (isNaN(convertSuperiorValue) || isNaN(convertInferiorValue)) return; //si no es un numero no hace nada
       switch (this.operator) {
         case "+":
           result = convertSuperiorValue + convertInferiorValue;
@@ -55,7 +54,9 @@ document.addEventListener("DOMContentLoaded", function () {// cuando se cargue e
         case "/":
           result = convertSuperiorValue / convertInferiorValue;
           break;
-        default:
+        case "%":
+          result = (convertSuperiorValue * convertInferiorValue) / 100;
+        default: //si no se cumple ninguno de los casos no hace nada
           return;
       }
       this.inferiorValue = result.toString();
@@ -92,7 +93,7 @@ document.addEventListener("DOMContentLoaded", function () {// cuando se cargue e
     calculator.compute();
     calculator.updateDisplay();
   });
-  
+
   buttonEraseAll.addEventListener("click", () => {
     calculator.eraseScreen();
     calculator.updateDisplay();
