2b04e90da0f881da0e0319163f8b2bf29de23938
dci-student <fredy.montalvo@dci.student>
2023-07-18 11:02:58 +0200

    Update

--- index.js
+++ index.js
@@ -6,39 +6,40 @@ const buttonErase = document.querySelector(".erase");
 const buttonEraseAll = document.querySelector(".erase-all");
 const textSuperiorValue = document.querySelector(".superior_value");
 const textInferiorValue = document.querySelector(".inferior_value");
-console.log(buttonNumm);
+
 
 class Calculator {
   constructor(textInferiorValue, textSuperiorValue) {
-    this.textInferiorValue = textInferiorValue;
+    this.textInferiorValue = textInferiorValue;// creamos variabeles locales para que no se confundan con las globales
     this.textSuperiorValue = textSuperiorValue;
     this.inferiorValue = "";
     this.superiorValue = "";
     this.operator = undefined; //para guardar la operacion que se va a realizar
   }
+  //metodos
   addNumber(number) {
     if (number === "." && this.inferiorValue.includes(".")) return; //si el numero es un punto y el valor inferior incluye un punto no hace nada.
     this.inferiorValue += number; //concatena el numero que se presiona
   }
   updateDisplay() {
-    this.textInferiorValue.textContent = this.inferiorValue;
+    this.textInferiorValue.textContent = this.inferiorValue;//actualiza el valor inferior
     this.textSuperiorValue.textContent = this.superiorValue;
   }
   erase() {
     this.inferiorValue = this.inferiorValue.slice(0, -1); //elimina el ultimo elemento de la cadena
   }
   chooseOperation(operator) {
-    if (this.inferiorValue === "") return; //
+    if (this.inferiorValue === "") return; //si el valor inferior esta vacio no hace nada
     if (this.superiorValue !== "") {
       this.compute(); //si el valor superior no esta vacio realiza la operacion
     }
     this.operator = operator; //guarda el operador
     this.superiorValue = this.inferiorValue; //guarda el valor inferior en el superior
-    this.inferiorValue = ""; //limpia el valor inferior
+    this.inferiorValue = ""; // borra el valor inferior
   }
   compute() {
-    let result;
-    let convertSuperiorValue = parseFloat(this.superiorValue); //convierte el valor superior en un numero
+    let result;//almacena los parametros de la operacion
+    let convertSuperiorValue = parseFloat(this.superiorValue); //convierte el string en un numero
     let convertInferiorValue = parseFloat(this.inferiorValue);
     if (isNaN(convertSuperiorValue) || isNaN(convertInferiorValue)) return; //si no es un numero no hace nada
     switch (this.operator) {
